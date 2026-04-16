ecb00a2f122c8365daae78904c4839e817f70eba
Жания Алиева <alievazania2004@gmail.com>
2026-04-17 01:28:33 +0400

    рабочая версия 2.0

--- src/main/java/ru/ssau/dto/JwtResponse.java
+++ src/main/java/ru/ssau/dto/JwtResponse.java
@@ -7,14 +7,12 @@ public class JwtResponse {
     private String token;
     private String type = "Bearer";
     private Long id;
-    private String username;
     private String fullName;
     private String role;
 
-    public JwtResponse(String token, Long id, String username, String fullName, String role) {
+    public JwtResponse(String token, Long id, String fullName, String role) {
         this.token = token;
         this.id = id;
-        this.username = username;
         this.fullName = fullName;
         this.role = role;
     }
