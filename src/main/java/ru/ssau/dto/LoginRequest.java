305a6fafd02c3ef3666a6f9e68248ffe5cca08fe
Жания Алиева <alievazania2004@gmail.com>
2026-04-20 13:05:05 +0400

    рабочая версия 3.0

--- src/main/java/ru/ssau/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package ru.ssau.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String fullname;
-    private String password;
-}
--- src/main/java/ru/ssau/repository/SubjectResultRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.ssau.repository;
-
-import ru.ssau.entity.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubjectResultRepository extends JpaRepository<SubjectResult, Long> {
-}
